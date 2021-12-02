03e4866561180df9394f97f4213043b9078026a3
somasundaram2000 <94282752+somasundaram2000@users.noreply.github.com>
2021-12-02 10:03:44 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,41 @@
+/**
+ * @file main.c
+ * @author Somasundaram V L
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "8x8Matrix.h"
+void reset_display(){
+	
+}
+int main(){
+  uint8_t key_press;
+  uint16_t *input;
+  uint16_t result;
 
+  lcd_init();
+  keypad_init();
+	
+  
+  while(1){
+	   key_press=keypad_scan();
+	   if(key_press !=0){	
+	   
+		if(key_press=='c'){
+			lcd_send_command(LCD_CMD_CLEAR_DISPLAY);
+			lcd_write_word("Enter Expression!");
+			lcd_goto_xy(1,0);
+			result=scan_and_eval();
+			print_result(result);
+			
+			}
+		}
+		
+   }
+
+  return 0;
+}
